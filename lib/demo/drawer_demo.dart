add3fbbed693914b97ba318be907f49541763b4b
EvanBell <876895046@qq.com>
2018-08-27 15:49:57 +0800

    添加顶部TAP切换功能和抽屉菜单列表

---
+++ lib/demo/drawer_demo.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class DrawerDemo extends StatelessWidget{
+	@override
+	Widget build(BuildContext context) {
+    return Drawer(
+			child: ListView(
+				padding: EdgeInsets.zero,
+				children: <Widget>[
+					UserAccountsDrawerHeader(
+						currentAccountPicture: CircleAvatar(
+							backgroundImage: NetworkImage('https://avatars2.githubusercontent.com/u/17161723?s=460&v=4'),
+						),
+						accountName: Text('Mugen_Uncle', style: TextStyle(fontWeight: FontWeight.bold),),
+						accountEmail: Text('yanlezhong@gmail.com'),
+						decoration: BoxDecoration(
+							color: Colors.yellow[400],
+							image: DecorationImage(
+								image: NetworkImage('https://tse1.mm.bing.net/th?id=OIP.JAAQvXxf131JjLxiCsF_bgHaEK&pid=Api'),
+								fit: BoxFit.cover, // 填充方式：铺满
+								colorFilter: ColorFilter.mode(
+									// 颜色滤镜方式
+									Colors.yellow[400].withOpacity(0.6),
+									BlendMode.hardLight,
+								)
+							),
+						),
+					),
+					ListTile(
+						title: Text('Messages', textAlign: TextAlign.right,),
+						trailing: Icon(Icons.message, color: Colors.black12, size: 22.0,),
+						onTap: () => Navigator.pop(context),
+					),
+					ListTile(
+						title: Text('Favorite', textAlign: TextAlign.right,),
+						trailing: Icon(Icons.favorite, color: Colors.black12, size: 22.0,),
+						onTap: () => Navigator.pop(context),
+					),
+					ListTile(
+						title: Text('Settings', textAlign: TextAlign.right,),
+						trailing: Icon(Icons.settings, color: Colors.black12, size: 22.0,),
+						onTap: () => Navigator.pop(context),
+					),
+				],
+			),
+		);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import './demo/listview_demo.dart';
+import './demo/drawer_demo.dart';
 
 void main() => runApp(App());
 
@@ -10,7 +11,9 @@ class App extends StatelessWidget {
       debugShowCheckedModeBanner: false, // 是否显示DEBUG标签
       home: Home(),
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
+        primarySwatch: Colors.yellow, // 主题颜色
+        highlightColor: Color.fromRGBO(255, 255, 255, 0.5), // 高亮背景颜色
+        splashColor: Colors.white70,  // 水波纹颜色
       ),
     );
   }
@@ -19,13 +22,56 @@ class App extends StatelessWidget {
 class Home extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[100],
-      appBar: AppBar(
-        title: Text('FLUTTER'),
-        elevation: 0.0,
-      ),
-      body: null,
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.grey[100],
+        appBar: AppBar(
+          // 标题
+          title: Text('FLUTTER'),
+
+//          // 左侧按钮
+//          leading: IconButton(
+//            icon: Icon(Icons.menu),
+//            tooltip: 'Navigration',
+//            onPressed: () => debugPrint('Navigration button is pressed.'),
+//          ),
+
+          // 右侧按钮组
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.search),
+              tooltip: 'Search',
+              onPressed: () => debugPrint('Search button is pressed.'),
+            ),
+          ],
+
+          // 默认高度
+          elevation: 0.0,
+
+          // 底部设置
+          bottom: TabBar(
+            unselectedLabelColor: Colors.black38, // 未激活图标的颜色
+            indicatorColor: Colors.black54,       // 激活的颜色
+            indicatorSize: TabBarIndicatorSize.label,   // 激活的下划线的大小
+            indicatorWeight: 1.0,                 // 激活的宽度
+            tabs: <Widget>[
+              Tab(icon: Icon(Icons.local_florist)),
+              Tab(icon: Icon(Icons.change_history)),
+              Tab(icon: Icon(Icons.directions_bike)),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: <Widget>[
+            Icon(Icons.local_florist, size: 128.0, color: Colors.black12,),
+            Icon(Icons.change_history, size: 128.0, color: Colors.black12,),
+            Icon(Icons.directions_bike, size: 128.0, color: Colors.black12,),
+          ],
+        ),
+        drawer: DrawerDemo(),
+        // endDrawer: Text('This is endDrawer'), // 右侧出现抽屉
+      )
     );
   }
 }
